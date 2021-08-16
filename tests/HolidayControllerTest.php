ab73bc18b3aa0f0787891ebb3375e135f87ad6c1
Temitope Olotin <olotintemitope@gmail.com>
2021-08-16 09:39:07 +0100

    add phpunit

---
+++ tests/HolidayControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace LazHoliday\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class HolidayControllerTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function test_it_works(): void
+    {
+        $this->assertTrue(true);
+    }
+}
